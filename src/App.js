9de33435dafa47c2d8bd510bf92ac0ada3a126f4
Drew Colgin <acolgin@insightsquared.com>
2019-06-23 21:33:30 -0500

    get from build

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Drew's stuff is cool
+          Edit <code>src/App.js</code> and save to reload. Holla at ya boi
         </p>
         <a
           className="App-link"
